d3c64456487ed5b123e8e74d2843b5f9d8d1d8a0
had3s13 <hlaingwinhtut.daraz@outlook.com>
2017-06-22 13:15:49 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 const BootBot = require('./lib/BootBot');
+const config = require('./exemples/config');
+const bot = new BootBot({
+  accessToken: 'FB_ACCESS_TOKEN',
+  verifyToken: 'FB_VERIFY_TOKEN',
+  appSecret: 'FB_APP_SECRET'
+});
+bot.hear(['hello','ht','hey'],(payload,chat) => {
+	chat.say('Hello there!');
+});
 
-module.exports = BootBot;
+bot.strat();
